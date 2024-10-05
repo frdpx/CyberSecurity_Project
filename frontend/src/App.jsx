0e3b6d669251d33d5666faa49d0499b536da10dd
Fareeda Pakdeechat <fareeda.pak@ku.th>
2024-10-06 04:55:11 +0700

    Create Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
@@ -11,7 +15,12 @@ const App = () => {
     {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
       <Navbar setShowLogin={setShowLogin}/>
-      
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
     </div>
     </>
   )
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '/.Header.css'
+
+const Header = () => {
+  return (
+    <div className='header' >
+      <div className="header-contents">
+        <h2>order your favourite food HERE</h2>
+        <p>Choosing a diverse menu involves selecting a variety of dishes from different cuisines, 
+            ingredients, and preparation styles. This ensures that the meal is interesting and caters 
+            to different tastes and dietary preferences</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
