5ba9f0b46277c3065699ada5299893ddd4b9fcc4
Fareeda Pakdeechat <fareeda.pak@ku.th>
2024-10-05 15:50:06 +0700

    Your new commit message

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src=''/>
+      
+    </div>
+  )
+}
+
+export default Navbar
