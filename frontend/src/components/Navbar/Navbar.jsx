95d6484d47294189fcc36cce41a497984e71cb19
Fareeda Pakdeechat <fareeda.pak@ku.th>
2024-10-09 06:23:52 +0700

    Create Cart Page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = ({setShowLogin}) => {
   return (
     <div className='navbar'>
 
-        <img src={assets.logo} alt="" className="logo" />
+        <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
 
         <ul className='navbar-menu'>
             <Link to='/' onClick={()=>setMenu("home")} className={menu=="home" ? "active":""}>home</Link>
@@ -21,7 +21,7 @@ const Navbar = ({setShowLogin}) => {
         <div className='navbar-right'>
             <img src={assets.search_icon} alt=""  />
             <div className='navbar-search-icon'>
-                <img src={assets.basket_icon} alt="" />
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                 <div className='dot'>
 
                 </div>
