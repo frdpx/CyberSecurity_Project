4e2531a2093362039cabc4d0c4af8191b91a5cb9
Fareeda Pakdeechat <fareeda.pak@ku.th>
2024-10-11 00:26:04 +0700

    Merge pull request #15 from frdpx/srk_branch
    create backend

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addFood } from '../controllers/foodController.js';
-import multer from "multer";
+import multer from "multer"
 
 const foodRouter = express.Router();
 
 // Image Storage Engine
+
 
 const Storage = multer.diskStorage({
     destination:"uploads",
