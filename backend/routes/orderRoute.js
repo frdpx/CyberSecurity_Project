4bf1a28c457d8ed067dc4544c3548ebb1e009c5e
Aisaraphorn18 <aaaisanillpradub@gmail.com>
2025-09-28 20:33:03 +0700

    add require role admin

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,16 +4,20 @@ import {
   listOrders,
   updateStatus,
   userOrders,
-  placeOrderCod,
+  placeOrderCod
 } from "../controllers/orderController.js";
-import { supabaseAuthMiddleware, authMiddleware } from '../middleware/auth.js';
+import {
+  supabaseAuthMiddleware,
+  authMiddleware,
+  requireRole
+} from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 
 orderRouter.use(supabaseAuthMiddleware);
 orderRouter.post("/userorders", authMiddleware, userOrders);
-orderRouter.post("/status", updateStatus);
-orderRouter.get("/list", listOrders);
-orderRouter.post("/placecod", placeOrderCod);
+orderRouter.post("/status", requireRole("admin"), updateStatus);
+orderRouter.get("/list", requireRole("admin"), listOrders);
+orderRouter.post("/placecod", requireRole("admin"), placeOrderCod);
 
 export default orderRouter;
