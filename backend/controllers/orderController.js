d80326eb3428619da0f37e663caa5f2dedc6c0c8
Aisaraphorn18 <aaaisanillpradub@gmail.com>
2025-09-28 20:00:17 +0700

    fix throw error example at placeOrderCod

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,23 +7,27 @@ import userModel from "../models/userModel.js";
 
 const placeOrderCod = async (req, res) => {
   try {
-    console.log("Placing order for user:", req.user._id);
-    const userId = req.user._id;
-    // console.log("Incoming order:", req.body);
+    console.log("Placing order for user:", req.user?.id);
+    const userId = req.user?.id;
+    console.log("Incoming order:", req.body);
 
     if (
       !req.body.items ||
       !Array.isArray(req.body.items) ||
       req.body.items.length === 0
     ) {
-      return res
-        .status(400)
-        .json({ success: false, message: "No items provided" });
+      console.error("No items provided in order");
+      // return res
+      //   .status(400)
+      //   .json({ success: false, message: "No items provided" });
+      throw new Error("No items provided");
     }
     if (!req.body.address) {
-      return res
-        .status(400)
-        .json({ success: false, message: "No address provided" });
+      console.error("No address provided in order");
+      // return res
+      //   .status(400)
+      //   .json({ success: false, message: "No address provided" });
+      throw new Error("No address provided");
     }
 
     const newOrder = new orderModel({
@@ -31,18 +35,25 @@ const placeOrderCod = async (req, res) => {
       items: req.body.items,
       amount: req.body.amount,
       address: req.body.address,
-      payment: true,
+      payment: true
     });
+    console.log("New order created:", newOrder);
 
     const saved = await newOrder.save();
-    // console.log("Order saved:", saved);
+    console.log("Order saved:", saved);
 
     await userModel.findByIdAndUpdate(userId, { cartData: {} });
 
     res.json({ success: true, message: "Order Placed" });
   } catch (error) {
     // console.log(" Error placing order:", error);
-    res.status(500).json({ success: false, message: "Error placing order" });
+    // res.status(500).json({ success: false, message: "Error placing order" });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: error.message || "Error placing order"
+      });
   }
 };
 
